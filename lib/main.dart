fb15a239b5b830568500376898f7574c62368298
movses555 <mmartirosyan69@gmail.com>
2022-02-06 19:22:49 +0400

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intermax_task_manager/Flutter%20Toast/flutter_toast.dart';
 import 'package:intermax_task_manager/ServerSideApi/server_side_api.dart';
@@ -10,7 +10,7 @@ import 'package:responsive_framework/responsive_framework.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(const MaterialApp(
     home: TaskManagerMainPage(),
   ));
