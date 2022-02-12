a83538762254277e972cfdde7faf5fe49dda3847
movses555 <mmartirosyan69@gmail.com>
2022-02-12 22:11:05 +0400

    Added support to add both simple user and brigade user

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ import 'package:responsive_framework/responsive_framework.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  //await Firebase.initializeApp();
   AwesomeNotifications().initialize(
       null,
       [
